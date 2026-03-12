8f28fa671ddec53d105b040a706766a93cf3a84f
myfirstangel <gamer987tok@gmail.com>
2026-03-12 20:10:36 +0300

    Add settings and servers view models

---
+++ WayVPN/Views/ServerView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WayVPN.Views;
+
+public partial class ServerView : UserControl
+{
+    public ServerView()
+    {
+        InitializeComponent();
+    }
+}
